402f90c863ce983033cdd49c1a92663af81a8a0b
Artur <arurgg@gmail.com>
2024-08-09 11:21:03 +0200

    add django-ldap in settings.py

--- main/ldap.py
+++ main/ldap.py
@@ -1,13 +1,26 @@
 from ldap3 import Server, Connection, ALL
+from ldap3 import MODIFY_ADD, MODIFY_DELETE, MODIFY_REPLACE
 
-
-server = Server("WINDC0001.mshome.net", use_ssl=False, get_info=ALL)
+server = Server("WINDC0001", use_ssl=False, get_info=ALL)
 
 conn = Connection(server, 'cn=admin1,cn=users,dc=mydomain,dc=com', 'Secret123', auto_bind=True)
 # connection test user
 # need to relate connection to login(session)
 # no TLS config applied yet
 
-conn.search('dc=users,dc=mydomain,dc=com', '(objectclass=person)')
+conn.search('cn=users,dc=mydomain,dc=com', '(objectclass=person)')
 conn.entries
 # output: [CN=admin1,CN=Users,DC=mydomain,DC=com]
+
+# create user test
+
+conn.add('cn=user1,cn=users,dc=mydomain,dc=com', attributes={'objectClass':  ['inetOrgPerson', 'posixGroup', 'top'], 
+                                                             'sn': 'user_sn', 
+                                                             'gidNumber': 0
+                                                             })
+
+# modify user test
+conn.modify('cn=user1,cn=users,dc=mydomain,dc=com',
+         {'description': [(MODIFY_REPLACE, ['description-replaced'])],
+          'sn': [(MODIFY_REPLACE, ['sn-replaced'])]
+          })
