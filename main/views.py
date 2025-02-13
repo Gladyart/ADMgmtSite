6a3212798c3ec8cb598d225a782cc21861b4fc43
Gladyart <arurgg@gmail.com>
2025-02-13 15:56:55 +0100

    added account control funtion to site, need to refactor code in views.py a bit to make it more readable

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib import messages
 from .ldap import DCConnection, ADUser, OUPath, userAttributes
 
@@ -20,15 +21,15 @@ def home(request):
 
         if not searched:
             messages.success(request, "Item not found")
-            return render(request, "search.html", {})
+            return render(request, "search.html", {"currentUser":currentUser})
         else:
-            return render(request, "search.html", {"searched":searched})
+            return render(request, "search.html", {"searched":searched, "currentUser":currentUser})
     else:
         
         searchParameters = f'(&(objectclass=person)(cn={currentUser}))'                    
         entry = conn.entries[0]
 
-        return render(request, "home.html", {"entry":entry})
+        return render(request, "home.html", {"entry":entry, "currentUser":currentUser})
 
 
 def search(request):
@@ -44,16 +45,30 @@ def search(request):
             conn.search(OUPath, searchParameters, attributes=['sAMAccountName'])                       
             searched = conn.entries
 
-            return render(request, "search.html", {"searched":searched})
+            return render(request, "search.html", {"searched":searched, "currentUser":currentUser})
     
     else:
-        return render(request, "search.html", {})
+        return render(request, "search.html", {"currentUser":currentUser})
 
 
-def userID(response, id):
+def userID(request, id):
 
     entry = ADUser(id)
-    
+
+    method = request.GET.get('method')
+    if method == 'method1':            
+        ADUser.enableUser(entry)
+        return HttpResponseRedirect(reverse('userID', args=(entry.sAMAccountName)))
+    elif method == 'method2':            
+        ADUser.disableUser(entry)
+        return HttpResponseRedirect(reverse('userID', args=(entry.sAMAccountName)))
+    elif method == 'method3':        
+        pass
+    elif method == 'method4':
+        ADUser.unlockUser(entry)
+    elif method == None:
+        pass
+        
    
-    return render(response, "AD_user.html", {"entry":entry})
+    return render(request, "AD_user.html", {"entry":entry, "currentUser":currentUser})
 
